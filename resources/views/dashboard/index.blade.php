92ca2c5ff0497662f679d2558a355abc0ec3100e
Lazuardi <lazuardi.reznnov@gmail.com>
2022-05-01 18:03:36 +0700

    Memodifikasi halaman index dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,7 +1,22 @@
 @extends('dashboard.layouts.main')
 
 @section('container')
+
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
   <h1 class="h2">Dashboard</h1>
+</div>
+<div class="container">
+
+
+  <div class=" mb-4 bg-light rounded-3">
+    <div class="container-fluid py-2 text-center">
+      <img src="/img/ardi.jpg" width="300" class="rounded-circle mb-3">
+      <h1 class="display-5 fw-bold">LAZUARDI</h1>
+      <p class="text-muted">Full Stage Developer | Administration</p>
+     
+    </div>
+  </div>
+
+
 </div>
 @endsection
